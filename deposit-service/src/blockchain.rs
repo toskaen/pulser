373c2d9e277876925179a5b6ad4331cb97bda5b9
toskaen <155739476+toskaen@users.noreply.github.com>
2025-03-23 19:44:36 -0300

    Delete deposit-service/src/blockchain.rs

--- deposit-service/src/blockchain.rs
+++
@@ -1,8 +0,0 @@
-use bdk_esplora::esplora_client::{self, AsyncClient};
-use common::error::PulserError;
-
-pub fn create_blockchain(url: &str) -> Result<AsyncClient, PulserError> {
-    esplora_client::Builder::new(url)
-        .build_async()
-        .map_err(|e| PulserError::ApiError(format!("Esplora client error: {}", e)))
-}
